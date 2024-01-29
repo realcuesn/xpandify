a4686838b180d017707f06598f6cf43f7bfe8d18
realcuesn <129620236+realcuesn@users.noreply.github.com>
2024-01-29 21:32:50 +0530

    feat:update

--- components/home-fourth.vue
+++ components/home-fourth.vue
@@ -4,8 +4,8 @@
             <h2 class="text-4xl lg:text-4xl xl:text-5xl font-medium">Featured Works</h2>
 
             <div class="w-fit gap-x-2 mt-6 lg:mt-0 lg:gap-x-5 flex items-center">
-                <img src="@/assets/icons/arrow-right.svg" class="bg-black h-14 rounded-full scale-75 lg:scale-100 p-3"
-                    alt="">
+                <img src="@/assets/icons/arrow-right.svg"
+                    class="bg-black h-14 rounded-full scale-75 lg:scale-100 p-3 invert" alt="">
                 <p class="text-xl lg:text-2xl font-medium">View Projects</p>
             </div>
         </div>
@@ -13,8 +13,8 @@
         <ul class="w-full grid sm:grid-cols-2 gap-10 mt-28">
             <li class="w-full">
                 <div class="w-full aspect-square lg:h-[47vh] rounded-md">
-                    <img class="h-full w-full object-cover rounded-md" src="/images/picture/invac.png"
-                        alt="Invac" />
+                    <NuxtImg preload placeholder format="webp" width="1000" class="h-full w-full object-cover rounded-md"
+                        src="/images/picture/invac.png" alt="Invac" />
                 </div>
                 <div class="w-full flex items-center justify-between mt-5">
                     <h2 class="font-medium uppercase text-lg">Invac</h2>
@@ -23,8 +23,8 @@
 
             <li class="w-full">
                 <div class="w-full aspect-square lg:h-[47vh] rounded-md">
-                    <img class="h-full w-full object-cover rounded-md" src="/images/picture/creationfurnishings.png"
-                        alt="Creations" />
+                    <NuxtImg preload placeholder format="webp" width="1000" class="h-full w-full object-cover rounded-md"
+                        src="/images/picture/creationfurnishings.png" alt="Creations" />
                 </div>
                 <div class="w-full flex items-center justify-between mt-5">
                     <h2 class="font-medium uppercase text-lg">Creations</h2>
@@ -33,8 +33,8 @@
 
             <li class="w-full">
                 <div class="w-full aspect-square lg:h-[47vh] rounded-md">
-                    <img class="h-full w-full object-cover rounded-md" src="/images/picture/dessangeindia.png"
-                        alt="Dessange" />
+                    <NuxtImg preload placeholder format="webp" width="1000" class="h-full w-full object-cover rounded-md"
+                        src="/images/picture/dessangeindia.png" alt="Dessange" />
                 </div>
                 <div class="w-full flex items-center justify-between mt-5">
                     <h2 class="font-medium uppercase text-lg">Dessange</h2>
@@ -43,8 +43,8 @@
 
             <li class="w-full">
                 <div class="w-full aspect-square lg:h-[47vh] rounded-md">
-                    <img class="h-full w-full object-cover rounded-md" src="/images/picture/in-air.png"
-                        alt="In air" />
+                    <NuxtImg preload placeholder format="webp" width="1000" class="h-full w-full object-cover rounded-md"
+                        src="/images/picture/in-air.png" alt="In air" />
                 </div>
                 <div class="w-full flex items-center justify-between mt-5">
                     <h2 class="font-medium uppercase text-lg">In air</h2>
--- components/home-second.vue
+++ components/home-second.vue
@@ -54,43 +54,43 @@
         <div class=" w-[60vw]  flex flex-col gap-y-3">
             <div class="w-full h-[37vw]">
                 <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
+                    src="/images/picture/pexels-antoni-shkraba-7144176.jpg" alt="Image 9" />
+            </div>
+            <div class="w-full h-[37vw]">
+                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
+                    src="/images/picture/pexels-asad-photo-maldives-1654834.jpg" alt="Image 9" />
+            </div>
+            <div class="w-full h-[37vw]">
+                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
+                    src="/images/picture/pexels-cottonbro-studio-3993320.jpg" alt="Image 9" />
+            </div>
+            <div class="w-full h-[37vw]">
+                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
+                    src="/images/picture/pexels-daniel-andraski-13065690.jpg" alt="Image 9" />
+            </div>
+        </div>
+        <div class=" w-[60vw]  flex flex-col gap-y-3">
+            <div class="w-full h-[37vw]">
+                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
+                    src="/images/picture/pexels-engin-akyurt-3331486.jpg" alt="Image 9" />
+            </div>
+            <div class="w-full h-[37vw]">
+                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
+                    src="/images/picture/pexels-pixabay-164634.jpg" alt="Image 9" />
+            </div>
+            <div class="w-full h-[37vw]">
+                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
                     src="/images/picture/pexels-maria-geller-2127039.jpg" alt="Image 9" />
             </div>
             <div class="w-full h-[37vw]">
                 <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
-                    src="https://via.placeholder.com/800x800" alt="Image 9" />
-            </div>
-            <div class="w-full h-[37vw]">
-                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
-                    src="https://via.placeholder.com/800x800" alt="Image 9" />
-            </div>
-            <div class="w-full h-[37vw]">
-                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
-                    src="https://via.placeholder.com/800x800" alt="Image 9" />
+                    src="/images/picture/pexels-pixabay-164634.jpg" alt="Image 9" />
             </div>
         </div>
         <div class=" w-[60vw]  flex flex-col gap-y-3">
             <div class="w-full h-[37vw]">
                 <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
-                    src="https://via.placeholder.com/800x800" alt="Image 9" />
-            </div>
-            <div class="w-full h-[37vw]">
-                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
-                    src="https://via.placeholder.com/800x800" alt="Image 9" />
-            </div>
-            <div class="w-full h-[37vw]">
-                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
-                    src="https://via.placeholder.com/800x800" alt="Image 9" />
-            </div>
-            <div class="w-full h-[37vw]">
-                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
-                    src="https://via.placeholder.com/800x800" alt="Image 9" />
-            </div>
-        </div>
-        <div class=" w-[60vw]  flex flex-col gap-y-3">
-            <div class="w-full h-[37vw]">
-                <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
-                    src="https://via.placeholder.com/800x800" alt="Image 9" />
+                    src="/images/picture/pexels-maria-geller-2127039.jpg" alt="Image 9" />
             </div>
             <div class="w-full h-[37vw]">
                 <NuxtImg preload placeholder format="webp" width="700" quality="80" class="h-full w-full object-cover"
