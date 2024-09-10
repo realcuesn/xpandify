aef391b52b110539eeafdf21076698587f2e7f41
realcuesn <129620236+realcuesn@users.noreply.github.com>
2024-09-10 10:31:49 +0530

    update-temp

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="w-full">
-        <AboutHero></AboutHero>
-        <AboutSecond></AboutSecond>
-        <AboutThird></AboutThird>
-        <!-- <div class="h-[300vh]"></div> -->
+    <div class="w-full px-5 xl:px-20 py-28 lg:py-40">
+        <div class="w-full lg:flex items-center justify-between">
+            <h2 class="text-4xl lg:text-4xl xl:text-6xl 2xl:text-7xl font-medium lg:pr-[30%]">Welcome to Xpandify! I'm Teja Bharath Polamarasetty, a freelance web developer and digital consultant specializing in creating engaging websites, SEO optimization, and digital strategies for businesses. Though my professional brand, Xpandify, is designed to be easily recognizable and shared, it represents the work I personally deliver to clients across the world. As a Freelancer, I offer web development, branding consultation, and Meta campaign setup to help businesses grow their online presence. If you're looking for tailored solutions and collaboration on your next digital project, feel free to reach out!</h2>
+        </div>
     </div>
 </template>
+
+  
 
 <script setup>
 useHead({
